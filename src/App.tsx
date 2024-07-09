7dcde1c96924e9f3962f3dd9aacac57a44f54822
tetra-min <minjun.lee@tetracom.co.jp>
2024-07-09 17:11:27 +0900

    add react app

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <div>Initialize project</div>;
+};
+
+export default App;
---
+++ webpack.common.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.tsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+    clean: true,
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts|tsx)$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './build',
+    hot: true,
+    open: true,
+  },
+};
